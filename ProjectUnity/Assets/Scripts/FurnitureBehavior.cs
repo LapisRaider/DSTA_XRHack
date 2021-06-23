19c04158bd8ca7b08af6fcb9dbb90d0c093383b5
LapisRaider <angielimenqi7@gmail.com>
2021-06-23 18:42:56 +0800

    - imported textmeshpro - include player tag on prefab

---
+++ ProjectUnity/Assets/Scripts/FurnitureBehavior.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FurnitureBehavior : MonoBehaviour
+{
+    public GameObject m_FurnitureUI = null;
+
+    private bool m_InFurnitureRadius = false;
+
+    private void Awake()
+    {
+        m_InFurnitureRadius = false;
+    }
+
+    // enter looking at furniture
+    public void EnterLookAtFurniture()
+    {
+        if (!m_InFurnitureRadius)
+            return;
+    }
+
+    // exit looking at furniture
+    public void ExitLookAtFurniture()
+    {
+        if (!m_InFurnitureRadius)
+            return;
+    }
+
+    // click on furniture
+    public void ClickedOnFurniture()
+    {
+        if (!m_InFurnitureRadius)
+            return;
+
+        //bring up UI
+        if (m_FurnitureUI == null)
+            return;
+
+        m_FurnitureUI.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            m_InFurnitureRadius = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            m_InFurnitureRadius = false;
+        }
+    }
+}
